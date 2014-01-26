1b8e202ab545b2ef1e7a6a4af2eb6f107d55a09c
Joe Violago <joe@honestbuildings.com>
2014-01-25 20:02:28 -0800

    001 php: cleanup

--- 001/001.php
+++ 001/001.php
@@ -4,19 +4,18 @@
 // 997 operations
 function multiples_foreach()
 {
-    $multiples = [];
-    $operations = 0;
+    $solution = $operations = 0;
 
     for($i = 3; $i < 1000; $i++)
     {
         if(($i % 5 == 0) || ($i % 3 == 0))
         {
-            $multiples[] = $i;
+            $solution += $i;
         }
         $operations++;
     }
 
-    return [$multiples, $operations];
+    return [$solution, $operations];
 }
 
 // Removes duplicates after merge
@@ -28,8 +27,10 @@ function multiples_range()
 
     $multiples = array_unique(array_merge($fives, $threes));
 
+    $solution = array_sum($multiples);
     $operations = count($fives) + count($threes);
-    return [$multiples, $operations];
+
+    return [$solution, $operations];
 }
 
 
@@ -42,9 +43,10 @@ function multiples_loops()
         array_diff(range(5,999,5), range(15,999,15))
     );
 
+    $solution = array_sum($multiples);
     $operations = count($multiples);
 
-    return [$multiples, $operations];
+    return [$solution, $operations];
 }
 
 
@@ -52,15 +54,14 @@ $trials = [];
 $attempts = [multiples_foreach(), multiples_range(), multiples_loops()];
 foreach ($attempts as $attempt)
 {
-    $start = array_sum(explode(" ",microtime()));
+    $start = array_sum(explode(" ", microtime()));
 
-    list($multiples, $operations) = $attempt;
-    $solution = array_sum($multiples);
+    list($solution, $operations) = $attempt;
 
-    $end = array_sum(explode(" ",microtime()));
-    $time = ($end - $start);
+    $end = array_sum(explode(" ", microtime()));
+    $milliseconds = ($end - $start) * 1000;
 
-    $trials[] = compact('solution', 'operations', 'time');
+    $trials[] = compact('solution', 'operations', 'milliseconds');
 }
 
 print_r($trials);
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Project Euler
-## 001 - Multiples of 3 and 5
+Attempts to code the most inefficient & efficient algorithms.
+
+### 001 - Multiples of 3 and 5
 - PHP (3 attempts)
