8c3873adc7a3fab107d2170866fc9edcca0581b3
Joe Violago <joe@honestbuildings.com>
2014-01-28 19:47:32 -0800

    004 ruby; whitespace

--- 004/004.rb
+++ 004/004.rb
@@ -1,34 +1,34 @@
 class Palindrome
-	def initialize(max_val)
-		$max = max_val
-	end
+    def initialize(max_val)
+        $max = max_val
+    end
 
-	def getMax
-		$i = $max
-		$operations = 0
-		$max_product = 0
-		while $i > 99 do
-			$j = $max
-			while $j > 99 do
-				$operations += 1
-				$product = $i * $j
+    def getMax
+        $i = $max
+        $operations = 0
+        $max_product = 0
+        while $i > 99 do
+            $j = $max
+            while $j > 99 do
+                $operations += 1
+                $product = $i * $j
 
-				if $product.to_s() == $product.to_s().reverse \
-					and $product > $max_product
+                if $product.to_s() == $product.to_s().reverse \
+                    and $product > $max_product
 
-					puts "#{ $i } x #{ $j } = #{ $product }"
-					$max_product = $product;
+                    puts "#{ $i } x #{ $j } = #{ $product }"
+                    $max_product = $product;
 
-				end
+                end
 
-				$j -= 1
-			end
+                $j -= 1
+            end
 
-			$i -= 1
-		end
+            $i -= 1
+        end
 
-		puts "Operations: #{ $operations }"
-	end
+        puts "Operations: #{ $operations }"
+    end
 end
 
 pally = Palindrome.new(999);
