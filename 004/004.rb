673cebe38c5cf1844b90b1a63e6bae3df137ab7e
Joe Violago <joe@honestbuildings.com>
2014-01-28 19:44:16 -0800

    004 ruby; solved

--- 004/004.rb
+++ 004/004.rb
@@ -1,21 +1,29 @@
 class Palindrome
 	def initialize(max_val)
 		$max = max_val
+		@products = {}
 	end
 
 	def getMax
 		$i = $max
 		$operations = 0
-		while $i > 0 do
-			$operations += 1
-			$product = $i * $max
+		$max_product = 0
+		while $i > 99 do
+			$j = $max
+			while $j > 99 do
+				$operations += 1
+				$product = $i * $j
+				# @hash["#{ $i }_#{ $j }"] = $product
 
-			if $product.to_s() == $product.to_s().reverse
-				puts "#{ $i } x #{ $max } = #{ $product }"
-				break
+				if $product.to_s() == $product.to_s().reverse && $product > $max_product
+					puts "#{ $i } x #{ $j } = #{ $product }"
+					$max_product = $product;
+				end
+
+				$j -= 1
 			end
 
-			$i-=1
+			$i -= 1
 		end
 
 		puts "Operations: #{ $operations }"
