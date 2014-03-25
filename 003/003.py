ea65fde89818e9509768d8c231a94b147bfd755b
Joe Violago <joe@honestbuildings.com>
2014-03-24 20:51:08 -0400

    003 python; solved

--- 003/003.py
+++ 003/003.py
@@ -1,48 +1,26 @@
-from math import sqrt
+def get_factors(n):
+    return set(
+        reduce(
+            list.__add__,
+            (
+                [i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0
+            )
+        )
+    )
 
-def divisorGenerator(n):
-    for i in xrange(1, n / 2 + 1):
-        if n % i == 0: yield i
-    yield n
+def is_prime(n):
+    # Memory Error
+    # for i in range(3, n):
+    for i in xrange(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-def primefactors(n):
-    '''lists prime factors, from greatest to smallest'''
-    i = 2
-    while i<=sqrt(n):
-        if n%i==0:
-            l = primefactors(n/i)
-            l.append(i)
-            return l
-        i+=1
-    return [n]      # n is prime
+factors = sorted(get_factors(600851475143), reverse=True)
 
+print(factors)
 
-def factorGenerator(n):
-    p = primefactors(n)
-    factors={}
-    for p1 in p:
-        try:
-            factors[p1]+=1
-        except KeyError:
-            factors[p1]=1
-    return factors
-
-def divisorGen(n):
-    factors = list(factorGenerator(n))
-    nfactors = len(factors)
-    f = [0] * nfactors
-    while True:
-        yield reduce(lambda x, y: x*y, [factors[x][0]**f[x] for x in range(nfactors)], 1)
-        i = 0
-        while True:
-            f[i] += 1
-            if f[i] <= factors[i][1]:
-                break
-            f[i] = 0
-            i += 1
-            if i >= nfactors:
-                return
-
-# for i in divisorGenerator(600851475143):
-for i in divisorGen(600851475143):
-    print i
+for factor in factors:
+    if is_prime(factor) == True:
+        print(factor)
+        break
